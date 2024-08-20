67d7b07c047dea66e3bdf876738f8152c8ea4957
AdityaDeshwal2002 <adityadeshwal02@gamil.com>
2024-08-20 14:49:37 +0530

    Updating the project<Wasn't included glide at all>

--- app/src/main/java/com/example/glideapp/MainActivity.java
+++ app/src/main/java/com/example/glideapp/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.glideapp;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +18,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        ImageView imageView = findViewById(R.id.imageView);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+       //Glide.with(MainActivity.this).load(R.drawable.dhivacar).into(imageView);
+       imageView.setImageResource(R.drawable.dhivacar);
     }
 }
